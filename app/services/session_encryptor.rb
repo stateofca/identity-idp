6e66dec36c6a6986cedd976e8b0953aa4f61f6c5
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-04-15 21:16:22 -0400

    Migrate Marhsalled sessions to JSON
    **Why**: Since we already have sessions in production that were
    serialized with Marshal, we need to be able to continue supporting them,
    and migrate them to the new JSON serializer. By logging the presence of
    Marshalled sessions, we can check the logs after a predefined amount of
    time to determine when it's safe to remove this migration code.
    
    The first 3 characters of the Marshal-serialized + Base64 encoded value
    are always the same as far as I can tell. I tested this locally and on
    the dev and qa servers. Since these are constant, we can check if the
    decrypted value passed into `SessionEncryptor.load` starts with those 3
    characters, and if so, it means it was serialized with Marshal. We can
    then serialize it with JSON. Eventually, all sessions that were
    serialized with Marshal will be converted to JSON.

--- app/services/session_encryptor.rb
+++ app/services/session_encryptor.rb
@@ -1,4 +1,6 @@
 class SessionEncryptor
+  MARSHAL_SIGNATURE = 'BAh'.freeze
+
   def self.build_user_access_key
     key = Figaro.env.session_encryption_key
     UserAccessKey.new(password: key, salt: key)
@@ -10,7 +12,17 @@ class SessionEncryptor
 
   def self.load(value)
     decrypted = encryptor.decrypt(value, user_access_key)
-    JSON.parse(decrypted, quirks_mode: true).with_indifferent_access
+
+    if decrypted.start_with?(MARSHAL_SIGNATURE)
+      Rails.logger.info 'Marshalled session found'
+      # rubocop:disable Security/MarshalLoad
+      Marshal.load(::Base64.decode64(decrypted)).tap do |decoded_value|
+        dump(decoded_value)
+      end
+      # rubocop:enable Security/MarshalLoad
+    else
+      JSON.parse(decrypted, quirks_mode: true).with_indifferent_access
+    end
   end
 
   def self.dump(value)
